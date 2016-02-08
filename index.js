85fa67a5054fdf2c9c6ecff1abba0535aab60d4b
Anthony Lukach <anthonylukach@gmail.com>
2016-02-07 23:34:05 -0700

    Simplify server-client syncing, cleanup code

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ console.log("Starting...")
 var compression = require('compression')
 var ds18x20 = require('ds18x20')
 var express = require("express")
-var gpio = require("gpio");
+var gpio = require("gpio")
 var http = require("http")
 var Primus = require("primus")
-var PIDController = require('node-pid-controller');
+var PIDController = require('node-pid-controller')
 
 var BroadcastLogs = require('./lib/broadcast-logs')
 var Controller = require('./lib/control-system')
@@ -14,14 +14,12 @@ var Controller = require('./lib/control-system')
 var port = process.env.PORT || 3000
 var power_ctrl_pin = process.env.PWR_CTRL_PIN || 17
 var temp_poll_interval = process.env.POLL_INTERVAL || 1000
-var last_temp
 var power_state = 0
-var controlId = {val: 0, running: false}
-var target_temp
-var ctrlr, pid_ctrlr
+var last_temp, target_temp, pid_ctrlr, ctrlr
 var k_p = 10
-var k_i = 300
-var k_d = 30
+var k_i = 600
+var k_d = 0
+
 
 // Setup temperature sensor
 function getTemp (sensor_id) {
@@ -36,7 +34,7 @@ function getTemp (sensor_id) {
     // Send data to each cnxn
     primus.forEach(function (spark) {
       // TODO: Send PID values with temp
-      spark.emit('temp', temp, new Date(), power_state);
+      spark.emit('temp', temp, new Date(), power_state)
     })
     last_temp = temp
   })
@@ -56,8 +54,8 @@ power_ctrl.on('change', function(val) {
   // Send data to each cnxn
   primus.forEach(function (spark) {
     spark.emit('power', val)
-  });
-});
+  })
+})
 
 // Setup app
 var app = express()
@@ -82,48 +80,51 @@ primus.on('connection', function (spark) {
   // Send initial state
   spark.emit('power', power_ctrl._get())
   spark.emit('temp', last_temp, new Date(), power_state)
-  spark.emit('runControl', controlId.running, target_temp)
+  console.log('ctrlr_state', ctrlr ? ctrlr.state : false)
+  spark.emit('ctrlr_state', ctrlr ? ctrlr.state : false)
 
   // Handle manual power override signal
   spark.on('power', function power (val) {
     power_ctrl.set(parseInt(val))
-  });
+  })
 
-  // Handle manual power override signal
-  spark.on('runControl', function runControl (state, target_temp) {
-    if (parseInt(state)) {
-      target_temp = parseInt(target_temp)
+  // Handle entering control loop
+  spark.on('ctrlr_state', function runControl (state) {
+    if (Boolean(state)) {
+      target_temp = parseInt(state.target_temp)
       var pid_ctrlr = new PIDController({
         k_p: k_p,
         k_i: k_i,
         k_d: k_d,
       })
       pid_ctrlr.setTarget(target_temp)
+
       ctrlr = new Controller({
-        goal_temp: target_temp,
+        target_temp: target_temp,
         pid_ctrlr: pid_ctrlr,
         power_ctrl: power_ctrl,
         socket_server: primus,
         last_temp: last_temp
       })
       ctrlr.start()
+      console.log(target_temp)
+      primus.forEach(function (spark) {
+        spark.emit('runControl', ctrlr ? ctrlr.isRunning : false, target_temp)
+      })
     } else {
-      clearTimeout(controlId.val)
-      controlId.val = 0
-      power_ctrl.set(0)
-      console.log("Stopped control cycle")
+      ctrlr.stop()
     }
-  });
+  })
 })
 primus.on('disconnection', function (spark) {
   console.log('client ' + spark.id + ' has disconnected to the server')
 })
 // Notify server when control value changes
-Object.observe(controlId, function (changes) {
-  controlId.running = controlId.val ? true : false
-  primus.forEach(function (spark) {
-    spark.emit("runControl", controlId.running)
-  })
-})
+// Object.observe(ctrlr.isRunning, function (changes) {
+//   // TODO: Update code to set controlId
+//   primus.forEach(function (spark) {
+//     spark.emit("runControl", ctrlr.isRunning)
+//   })
+// })
 
 console.log("Ready to Crock!")
